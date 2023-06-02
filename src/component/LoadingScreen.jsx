6736ee2f262e2dc825edc0b6da636908a2d4453d
DESKTOP-EPM5P8O\skyline <nipunchathuranga45@gmail.com>
2023-06-02 13:39:00 +0530

    CHG | add loading screen

--- src/component/LoadingScreen.jsx
+++ src/component/LoadingScreen.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 import styles from '../styles/loadingBar.module.css'
 export default function Loading() {
   return (
-    <div className={styles.loading}>Loading..........</div>
+    <div className={styles.loadingScreen}>
+      <div>Loading.......... </div>
+     <div className={styles.loadingSpinner}></div>
+     </div>
   )
 }
